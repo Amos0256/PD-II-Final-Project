ee2e7013eb6c0b49b83b5ce9a242287eee94a246
Amos0506 <amos50308@gmail.com>
2022-05-26 16:28:44 +0800

    20220526 by Amos

--- myALGO.c
+++ myALGO.c
@@ -1,7 +1,8 @@
 #include "basic.h"
 #include "myALGO.h"
 
-int room[MAX_ROOM] = {0};
+//room is a queue
+HeadNode room[MAX_ROOM];
 
 /*     sort compare functions     */
 //AGE大到小，若一樣，病情1到5
@@ -36,7 +37,9 @@ int Cmp_situation(const void* a, const void* b){
 
 /*    clinic select     */
 int Situation_value(char* situ){
-  if(strcmp(situ, "CARACCIDENT") == 0)
+  if(strcmp(situ, "PANDMIC") == 0)
+    return 0;
+  else if(strcmp(situ, "CARACCIDENT") == 0)
     return 1;
   else if(strcmp(situ, "PREGNANT") == 0)
     return 2;
@@ -46,18 +49,29 @@ int Situation_value(char* situ){
     return 4;
   else if(strcmp(situ, "SICK") == 0)
     return 5;
+  /*
   #if PANDEMIC_MODE
   else if(strcmp(situ, "PANDMIC") == 0)
     return 0;
   #endif
+  */
+}
+
+void Room_init(HeadNode *room){
+  for(int i = 0; i < MAX_ROOM; i++)
+    room[i] = {.num = 0, .first = NULL, .last = NULL};
 }
 
 int Judge(int r1, int r2){
+  /*
   #if PANDMIC_MODE
+  */
   return MIN(r1, r2);
+  /*
   #elif !PANDMIC_MODE
   return MIN_General(MIN_General(r1, r2), 6);
   #endif
+  */
 }
 
 void Select_room(Inform patient){
@@ -65,23 +79,42 @@ void Select_room(Inform patient){
   int clinic_num;
   switch (value){
   case 0:
-    clinic_num = Judge(6, 6);
+    clinic_num = 0;
     break;
   case 1:
-    clinic_num = Judge(0, 4);
-    break;
-  case 2:
-    clinic_num = Judge(0, 1);
-    break;
-  case 3:
     clinic_num = Judge(1, 2);
     break;
-  case 4:
+  case 2:
     clinic_num = Judge(2, 3);
     break;
+  case 3:
+    clinic_num = Judge(3, 4);
+    break;
+  case 4:
+    clinic_num = Judge(4, 5);
+    break;
   case 5:
-    clinic_num = Judge(3, 4); 
+    clinic_num = Judge(5, 1); 
     break;
   }
-  room[clinic_num]++;
+}
+
+void Push_room(HeadNode room, Inform patient){
+  QueueNode* new_node = (QueueNode*)malloc(sizeof(QueueNode));
+  new_node->patient = patient;
+  new_node->next = NULL;
+
+  if(room.num == 0){
+    room.first = new_node;
+    room.last = new_node;
+  }
+  else{
+    QueueNode* temp = room.last;
+    temp->next = new_node;
+    room.last = new_node;
+  }
+  room.num++;
+}
+
+void Pop_room(HeadNode room){
 }
--- myALGO.h
+++ myALGO.h
@@ -2,10 +2,21 @@
 #define MYALGO_H
 
 #define MAX_ROOM 6
-#define PANDEMIC_MODE 0
+//#define PANDEMIC_MODE 0
 #define MAX_Cmpfunc(a, b) ((a) >= (b) ? 1 : -1)
 #define MIN_Cmpfunc(a, b) ((a) <= (b) ? 1 : -1)
 #define MIN_General(a, b) ((a) <= (b) ? (a) : (b))
+
+typedef struct{
+  Inform patient;
+  QueueNode* next;
+}QueueNode;
+
+typedef struct{
+  int num;
+  QueueNode* first;
+  QueueNode* last;
+}HeadNode;
 
 int Cmp_age(const void* a, const void* b);
 int Cmp_weight(const void* a, const void* b);
@@ -13,5 +24,7 @@ int Cmp_situ(const void* a, const void* b);
 int Situation_value(char* situ);
 int Judge(int r1, int r2);
 void Select_room(Inform patient);
+void Push_room(HeadNode room, Inform patient);
+void Pop_room(HeadNode room);
 
 #endif
